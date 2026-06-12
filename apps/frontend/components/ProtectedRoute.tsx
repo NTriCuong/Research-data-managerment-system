423abdb7a39608ef4f5af55b8d557cfa71ff30f2
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2026-06-12 12:35:48 +0700

    Ft bảo mật trang phía frontend chặn các user không được quá giớ hạng không được truy cập các path không thuôc quyền hạn

--- apps/frontend/components/ProtectedRoute.tsx
+++ apps/frontend/components/ProtectedRoute.tsx
@@ -1,44 +1,36 @@
-// components/ProtectedRoute.tsx
-
 "use client";
 
 import { useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { hasPermission } from "@/lib/auth/permissions";
 import { useAppSelector } from "@/store/hooks";
-import { selectCurrentUser, selectIsAuthenticated } from "@/store/slice/auth.slice";
+import { selectCurrentUser, selectIsAuthenticated, selectIsInitializing } from "@/store/slice/auth.slice";
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
 }
 
-export default function ProtectedRoute({
-    children
-}: ProtectedRouteProps) {
-
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
     const router = useRouter();
     const pathname = usePathname();
-
+    const isInitializing = useAppSelector(selectIsInitializing);
     const isAuthenticated = useAppSelector(selectIsAuthenticated);
-
     const user = useAppSelector(selectCurrentUser);
 
     useEffect(() => {
+        if (isInitializing) return;
 
-        // chưa login
         if (!isAuthenticated) {
             router.replace("/login");
             return;
         }
 
-        if (
-            user &&
-            !hasPermission(user.role_name, pathname)
-        ) {
+        if (user && !hasPermission(user.role_name, pathname)) {
             router.replace("/403");
         }
+    }, [isInitializing, isAuthenticated, user, pathname]);
 
-    }, [isAuthenticated, user, pathname]);
+    if (isInitializing) return null;
 
     return <>{children}</>;
 }
